6b333e1e6cf2a6b8acfdf1d4de4b6aceee54fed5
Osvaldo San Juan <osvaldosj@gmail.com>
2026-03-05 17:30:46 -0300

    fix puerto railway

--- backend/server.js
+++ backend/server.js
@@ -95,7 +95,7 @@ app.get("/obra/:id", async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Dashboard Estadisticas
 app.get("/dashboard", async (req, res) => {
