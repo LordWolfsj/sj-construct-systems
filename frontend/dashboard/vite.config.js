9b2d8adef7366fafd8c05c2e968be8b55f73a254
Osvaldo San Juan <osvaldosj@gmail.com>
2026-03-27 12:29:56 -0300

    fix vite preview allowed hosts

--- frontend/dashboard/vite.config.js
+++ frontend/dashboard/vite.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   preview: {
     host: true,
-    allowedHosts: ['sj-construct-systems-production.up.railway.app']
+    allowedHosts: "all",
   }
 })
